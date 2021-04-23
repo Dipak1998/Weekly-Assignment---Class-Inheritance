059fa69f01c4c3af14aba108cef0f949aa6eb38f
Dipak Garai <garaidipak1998@gmail.com>
2021-04-23 15:27:51 +0530

    upload circle.js and shape.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,31 @@
 
 
 // export class using module.exports
+// import {Shape} from './shape.js';
+var Shape = require('./shape.js')
+
+class Circle extends Shape{
+    
+    constructor(d){
+        super(d);
+        this.r = d;
+    }
+    drawShape(color){
+        var color = 'red';
+        return color;
+    }
+    calculateArea(){
+        var area = ( this.r)*( this.r)* Math.PI;
+        return area;
+
+    }
+}
+
+
+let c = new Circle(3);
+var result = c.calculateArea();
+var result2 = c.drawShape();
+
+console.log(result);
+console.log(result2);
+
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
 
 // export class using module.exports
+class Shape{
+    constructor(a,b){
+        this.base = a;
+        this.height = b;
+    }
+    drawShape(color){
+        var color = 'red';
+        return color;
+        
+    }
+    calculateArea(){
+        var arae = base * height;
+        return arae;
+    }
+}
+export default Shape;
